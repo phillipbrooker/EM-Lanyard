612684b5caf4c8fd9f78f23f6af997f4c098e226
Phillip Brooker <p.d.brooker@liverpool.ac.uk>
2024-05-15 11:49:20 +0100

    Add files via upload

---
+++ undisciplining_msgs.py
@@ -0,0 +1,8 @@
+msgs = [("Trying to tag on to", "someone's dinner", "plans"),
+        ("Going to see a", "talk that's not", "relevant to me"),
+        ("Trying to meet a", "professional hero", " "),
+        ("Finding a sounding", "board for a", "research problem"),
+        ("Going with the flow,", "so what are you", "doing next?"),
+        ("Taking the", "conference outdoors.", "You coming?"),
+        ("Discussing a paper", "over a hot drink.", "With you?"),
+        ("On the hunt for", "new research", "collaborators")]
